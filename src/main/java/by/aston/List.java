e806a8ed91a09c132dc085ab3b9d90074156cd6e
Sasha <poetitp@gmail.com>
2024-02-08 03:39:17 +0300

    Added List

---
+++ src/main/java/by/aston/List.java
@@ -0,0 +1,15 @@
+package by.aston;
+
+
+public interface List<T> {
+    boolean add(T t);
+    T get(int index);
+    T remove(int index);
+    boolean addAll(List<? extends T> collection);
+    //Task 1
+
+
+    void resize();
+    boolean isEmpty();
+    int indexOf(T t);
+}
